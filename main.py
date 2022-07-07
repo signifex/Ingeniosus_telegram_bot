f7bce45ef1f7934af92d61ab6c5de4c83934beda
Ingeniosus <shamaevmv@gmail.com>
2022-07-07 23:00:18 +0200

    empty message

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(telegram_bot_token)
 
 commands_list = []
 
-@bot.message_handler(commands = ["start","help","find"])
+@bot.message_handler(commands = ["start","help"])
 def response(message):
     
     if message.text == "/start":
@@ -16,8 +16,8 @@ def response(message):
     if message.text == "/help":
         bot.send_message(message.chat.id, f"Here is list of all commands: {commands_list}")
         
-    if message.text == "/find":
-        bot.send_message(message.chat.id, "give me name or simbol of your security")
+   # if message.text == "/find":
+    #    bot.send_message(message.chat.id, "give me name or simbol of your security")
         
 @bot.message_handler(func=lambda message: True)
 def response_security(message):
@@ -40,7 +40,9 @@ def create_link_stock_api(**kwargs):
     link = "https://www.alphavantage.co/query?"
     for key, value in kwargs.items():
             link += "{0}={1}&".format(key, value)
-    link += f"apikey={stock_token}"
+
+
+            link += f"apikey={stock_token}"
     return link
 
 def format_answer_telegram(row_data):
@@ -52,58 +54,9 @@ def format_answer_telegram(row_data):
             row_answer.append(f"{i[3:].capitalize()}: {row_data[i]}")
     answer = "\n".join(row_answer)
     return answer
-
-#for i in x["bestMatches"][0].keys(): print(f'{i[3:]}: {x["bestMatches"][0][i]}')
-
-"""
-
-@bot.message_handler(commands = commands_list)
-def response(message):
-    
-    if message == "start":
-        bot.send_message(message.chat.id, "Hey, im stock viewer bot\n" + f"Here is list of all commands: {commands_list}")
-        
-    if message == "help":
-        bot.send_message(message.chat.id, f"Here is list of all commands: {commands_list}")
-        
-    if message == "find":
-        bot.send_message(message.chat.id, "give me name or simbol of your security")
-        echo_find(message)
-
-
-    
-@bot.message_handler(func=lambda message: True)
-def echo_find(message):
-    
-    answer = get_stock_market_data(message.text)
-    bot.send_message(message.chat.id, json.dumps(answer))
-
-
-
-
-
-
-# Handles all text messages that match the regular expression
-@bot.message_handler(regexp="SOME_REGEXP")
-def handle_message(message):
-	pass
-
-
-dp = Dispatcher(Bot(telegram_bot_token))
-
-@dp.message_handler(commands="start", "write todo", "search stock")
-async def start(message: types.Message):
-    start_buttons = ["weather", "stocks", "pull requests"]
-    keyboard = types.ReplyKeyboardMarkup()
-    keyboard.add(*start_buttons)
-    await message.answer(f"hey, im ur bot! \nhere is weather trendings:{get_weather}", reply_markup=keyboard)
-"""
-
     
 def main():
     bot.infinity_polling()
-
-
 
 if __name__ == "__main__":
     main()
